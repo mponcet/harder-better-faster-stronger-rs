70c778a68d8be5f3f7cb4aa51d60dc49cfdacf75
Maxime Poncet <maxime.poncet@gmail.com>
2026-03-01 18:58:56 +0100

    use simd for station name comparision
    time:
    real    0m18,853s
    user    0m12,136s
    sys     0m4,217s

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
-// use std::collections::HashMap;
 use hashbrown::HashMap;
 use std::fs::File;
+use std::hash::Hash;
 use std::os::fd::AsRawFd;
+
+#[derive(Eq)]
+struct Station<'a>(&'a [u8]);
 
 struct Weather {
     samples: u32,
     min: i16,
     mean: i64,
     max: i16,
+}
+
+impl<'a> Hash for Station<'a> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.hash(state);
+    }
+}
+
+impl<'a> PartialEq for Station<'a> {
+    #[cfg(target_feature = "avx2")]
+    fn eq(&self, other: &Self) -> bool {
+        use std::arch::x86_64::*;
+
+        if self.0.len() != other.0.len() {
+            return false;
+        }
+        let diff = unsafe {
+            let lhs: __m256i = _mm256_loadu_si256(self.0.as_ptr() as *const __m256i);
+            let rhs: __m256i = _mm256_loadu_si256(other.0.as_ptr() as *const __m256i);
+            _mm256_movemask_epi8(_mm256_cmpeq_epi8(lhs, rhs)) as u32
+        };
+
+        let mask = (1 << self.0.len()) - 1;
+
+        diff & mask == mask
+    }
 }
 
 #[inline]
@@ -70,7 +99,7 @@ fn main() {
         libc::madvise(addr, len, libc::MADV_SEQUENTIAL);
         std::slice::from_raw_parts(addr as *const u8, len)
     };
-    let mut stats: HashMap<&str, Weather> = HashMap::with_capacity(10_000);
+    let mut stats: HashMap<Station, Weather> = HashMap::with_capacity(10_000);
 
     while !buf.is_empty() {
         if buf[0] == b'#' {
@@ -80,9 +109,10 @@ fn main() {
         buf = remainder;
         let temperature = parse_temperature(temperature);
 
-        let city = unsafe { str::from_utf8_unchecked(city) };
+        // let city = unsafe { str::from_utf8_unchecked(city) };
+        let city = Station(city);
         stats
-            .entry_ref(city)
+            .entry(city)
             .and_modify(|entry| {
                 if temperature < entry.min {
                     entry.min = temperature;
@@ -105,7 +135,7 @@ fn main() {
         .into_iter()
         .map(|(city, weather)| {
             (
-                city,
+                unsafe { str::from_utf8_unchecked(city.0) },
                 weather.min as f64 / 10.0,
                 (weather.mean as f64 / 10.0 / weather.samples as f64),
                 weather.max as f64 / 10.0,
